7229c5b56ce326ff61e00c2567254616e8e629d3
annika6246 <annikasalpukas@gmail.com>
2023-12-04 08:31:47 -0500

    disease spread

---
+++ Source/bio_features/disease_spread.h
@@ -0,0 +1,14 @@
+
+
+#ifndef FINAL_FOR_CS3520_DISEASE_SPREAD_H
+#define FINAL_FOR_CS3520_DISEASE_SPREAD_H
+
+#include <iostream>
+#include <mlpack/core.hpp>
+#include <mlpack/methods/linear_regression/linear_regression.hpp>
+
+void predictSpread(const std::string& filename);
+
+
+
+#endif //FINAL_FOR_CS3520_DISEASE_SPREAD_H
--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,7 @@
 #include "Source/bio_features/geneML.h"
+#include "Source/bio_features/disease_spread.h"
 
 int main() {
-    runClassifier("antibiotic_gene_data.csv");
-
+    //runClassifier("antibiotic_gene_data.csv");
     return 0;
 }
