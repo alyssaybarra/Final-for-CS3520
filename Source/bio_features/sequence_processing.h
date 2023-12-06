0278f5e76f83dc9a77de020b006c008e63cfa648
annika6246 <annikasalpukas@gmail.com>
2023-12-06 00:23:58 -0500

    sequence alignment scores

---
+++ Source/bio_features/sequence_processing.h
@@ -0,0 +1,15 @@
+
+#ifndef FINAL_FOR_CS3520_SEQUENCE_PROCESSING_H
+#define FINAL_FOR_CS3520_SEQUENCE_PROCESSING_H
+
+#include <iostream>
+#include <string>
+#include <seqan/align.h>
+#include <seqan/basic.h>
+#include <seqan/sequence.h>
+#include <seqan/stream.h>
+#include <seqan/seq_io.h>
+
+void processSequences(const std::string& inputFile);
+
+#endif //FINAL_FOR_CS3520_SEQUENCE_PROCESSING_H
--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,10 @@
 #include "Source/bio_features/geneML.h"
 #include "Source/bio_features/temp_analysis.h"
+#include "Source/bio_features/sequence_processing.h"
 
 int main() {
     //runClassifier("antibiotic_gene_data.csv");
-    SSTAnalysis("noaa_oni_values.csv");
+    //SSTAnalysis("noaa_oni_values.csv");
+    //processSequences("myoglobin.fasta");
     return 0;
 }
